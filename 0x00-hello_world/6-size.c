e161c96430025f6e4c0dee1a9dbe6c17ca9e85e8
Sydney Ozegbe <far2come@hotmail.com>
2023-02-17 15:32:47 +0000

    Size of datatype function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * sizeof - Size of datatype
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n" sizeof(long));
+	print("Size of a long long int: %d byete(s)\n" sizeof(long long));
+	print("Size of a float: %d bytes(s)\n" sizeof(float));
+}
