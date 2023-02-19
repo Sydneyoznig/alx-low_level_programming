9c6b4c5d4a965e6d5ffcf702a64d11f89482ca1d
Sydney Ozegbe <far2come@hotmail.com>
2023-02-19 22:39:05 +0000

    Update on lowercase and Uppercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar("abcdefghijklmnopqrstuvwxyz"[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
