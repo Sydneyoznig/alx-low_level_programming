f0d6f1ed0f0de756f82fa641a060e4eed5c2705f
Sydney Ozegbe <far2come@hotmail.com>
2023-02-23 22:08:27 +0000

    Print lower case alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	while (count <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
