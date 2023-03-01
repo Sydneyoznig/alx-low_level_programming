8f955dd26213896b63a49bc380e3c6a75c2bd5fd
Sydney Ozegbe <far2come@hotmail.com>
2023-03-01 11:14:04 +0000

    Check for uppercase or lower case character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _isalpha - Checks for uppercase
+ * Return: 1 if uppercase, 0 otherwise
+ * @letter: ASCII code value
+ */
+int _isalpha(int letter)
+{
+	if ((letter >= 65 && letter <= 90) || (kletter >= 97 && letter <= 122))
+		return (1);
+	return (0);
+}
