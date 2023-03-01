d05eb0b5834183be80a2a4617fc7ba6bc14d4c3a
Sydney Ozegbe <far2come@hotmail.com>
2023-03-01 12:26:23 +0000

    Update 13 on 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		putchar(43 +n);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar(45);
+		putchar(45 +n);
 		return (-1);
 	}
 	else
 	{
-		putchar(48);
+		putchar(48 +n);
 		return (0);
 	}
 }
