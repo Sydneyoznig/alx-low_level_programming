c2881335edb095e5498e3a0e396f5a960042c2a6
Sydney Ozegbe <far2come@hotmail.com>
2023-03-05 20:16:52 +0000

    Compute absolute integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Compute absolute integer value
+ * @i: Integer input
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
