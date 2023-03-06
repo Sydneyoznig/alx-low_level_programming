89bd6d475c2eac3db1adae1efb95a4845e88fbba
Sydney Ozegbe <far2come@hotmail.com>
2023-03-06 00:11:09 +0000

    Fibonacci numbers not exceeding 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Fibonacci values not exceeding 4000000
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	unsigned long num1 = 0, num2 = 1, num3 = 0, sum = 0;
+
+	while (num2 <= 4000000)
+	{
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		if ((n1 % 2) == 0)
+			sum += num1;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
